0c70bb7aeb5c0b24afb2c178aebb0d968aa1683f
Ma4kaigrisho <ivanovnikola0021@gmail.com>
2025-02-10 22:18:45 +0200

    14-artifacts adding another artifact that stores the test data

--- 14-artifacts/react-app/vite.config.ts
+++ 14-artifacts/react-app/vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true, // Enables global test functions like `test` and `expect`
-    environment: "jsdom", // Required for React component testing
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      provider: "v8", // Enables coverage reporting
+      reporter: ["text", "lcov"], // Generates reports in different formats
+    },
   },
-})
+});
