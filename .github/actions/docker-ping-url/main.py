ad2f33a5199be8e77448c27b1253936dfb138690
Ma4kaigrisho <ivanovnikola0021@gmail.com>
2025-02-22 12:54:57 +0200

    17-3-custom-actions-docker minor changes

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -5,11 +5,16 @@ import time
 def ping_url(url, max_trials, delay):
     
     for i in range(max_trials):
-        response = requests.get(url)
-        if(response.status_code == 200):
-            return True
-        else:
+        try:
+            response = requests.get(url)
+            if(response.status_code == 200):
+                return True
+        except requests.ConnectionError:
+            print(f"Website {url} is unreachable. Retrying in {delay} seconds...")
             time.sleep(delay)
+        except requests.exceptions.MissingSchema:
+            print(f"Invalid URL format")
+            return False
     return False
 
 def run():
@@ -20,5 +25,6 @@ def run():
     website_reachable = ping_url(website_url, delay, max_trials)
     if( not website_reachable):
         raise Exception("Website not reachable")
+    print("Website is reachable")
 if __name__ == "__main__":
     run()
